c1fcc1ea4e92db8b7f3e2bc28227b3e607c45df6
jbradgallagher@gmail.com <jbgallag@flash.uchicago.edu>
2015-10-23 13:22:18 -0500

    many updates and improvements, have began adding gesture types, pop and hit work know and there are duration and sensitivity controls, major update

--- SoundShapesFallingDots/SoundShape.js
+++ SoundShapesFallingDots/SoundShape.js
@@ -12,12 +12,20 @@ SoundShape = function(id,cContext, aContext, center, tone, radius, nShapes) {
     SINGLE: 0,
     LINEARY: 1
     };
-    this.shapeActionType = {
+    this.gestureResponseType = {
+    NONE: 0,
+    POP: 1,
+    HIT: 2,
+    HITONOFF: 3,
+    HOLD: 4
+    };
+    this.gestureTriggerType = {
     POP: 0,
     HIT: 1,
     HITONOFF: 2,
     HOLD: 3
     };
+
     this.numShapesInGroup = nShapes;
     this.center = center;
     this.radius = radius;
@@ -39,34 +47,29 @@ SoundShape = function(id,cContext, aContext, center, tone, radius, nShapes) {
     this.originX = this.center.x;
     this.originY = this.center.y;
     this.startTime = 0;
-    this.entropyChange = 0.0;
     this.soundShapeIsReady = -1;
     this.diffSum = 0.0;
     this.diffSumTol = 750.0;
     
     this.toneSave = 0.0;
     this.amplitude = 0.1;
-    this.connections = [];
+
     this.xoffset = 65.0;
     this.yoffset = 45.0;
     
     this.radiusDiv = 20.0;
     
-    this.poppedCount = 0;
-    this.popMax = 4;
     this.width = 640;
     this.height = 480;
     
     //this.motionType = 0; //0 = static, 1 = linear, 2 = gravity
-    this.motionIsLinear = false;
     this.yVelocity = 0.0;
-    this.yVelocitySave = this.yVelocity;
+    
     this.gravity = 200.0;
-    this.gravitySave = this.gravity;
     
     this.toneToPlay = this.tone;
     
-    this.restartTime = 1000.0;
+    this.actionDuration = 1000.0;
     this.randomizeRestartTime = false;
     
     this.randomizeXStartPos = false;
@@ -76,16 +79,19 @@ SoundShape = function(id,cContext, aContext, center, tone, radius, nShapes) {
     this.myMotionType;
     this.myColorType = this.colorType.SINGLE;
     this.myColor;
+    this.myHitColor;
    this.setShapeColor();
     
     this.setTone(tone);
     
     this.gestureTriggered = false;
+    this.myGestureResponseType = this.gestureResponseType.NONE;
+    this.myGestureTriggerType = this.gestureTriggerType.POP;
+    
+    this.circlePopped = false;
+    this.circleHit = false;
 }
 
-SoundShape.prototype.connect = function(node) {
-    this.connections.push(node);
-}
 
 SoundShape.prototype.setRandomizeXPos = function(rposx) {
     this.randomizeXStartPos = rposx;
@@ -104,8 +110,49 @@ SoundShape.prototype.setAmplitude = function(amplitude) {
 }
 
 SoundShape.prototype.setYVelocity = function(vel) {
-    this.yVelocitySave = this.yVelocity;
     this.yVelocity = vel;
+}
+
+SoundShape.prototype.setGravity = function(grav) {
+    this.gravity = grav;
+}
+
+SoundShape.prototype.setActionDuration = function(dur) {
+    this.actionDuration = dur;
+}
+
+SoundShape.prototype.setGestureTol = function(tol) {
+    this.diffSumTol = tol;
+}
+
+SoundShape.prototype.setCenter = function(p) {
+    this.center = p;
+    this.currentOriginX = this.center.x;
+    this.currentOriginY = this.center.y;
+}
+
+SoundShape.prototype.setTone = function(t) {
+    this.tone = t * Math.pow(1.05946,this.Ionian[this.myId]);
+    console.log("Tone is: ",this.tone);
+}
+
+
+SoundShape.prototype.setRadius = function(r) {
+    this.radius = r;
+}
+
+SoundShape.prototype.setShapeColor = function() {
+    var frequency = Math.PI/this.numShapesInGroup;
+    red   = Math.floor(Math.sin(frequency * this.myId + 2) * 127 + 128);
+    green = Math.floor(Math.sin(frequency * this.myId + 0) * 127 + 42);
+    blue  = Math.floor(Math.sin(frequency * this.myId + 4) * 127 + 128);
+    this.myColor = "rgba(" + red + "," + green + "," + blue + ", 0.8)";
+    //set hitColor
+    //red > green ? red > blue ? red = red + 30 : blue = blue + 30: green > blue ? green = green + 30 : blue = blue + 30;
+    red = red+50;
+    blue= blue+50;
+    green = green+50;
+    this.myHitColor = "rgba(" + red + "," + green + "," + blue + ", 0.8)";
 }
 
 SoundShape.prototype.setDefaults = function(sTime) {
@@ -123,69 +170,6 @@ SoundShape.prototype.setDefaults = function(sTime) {
     
     this.originX = this.center.x;
     this.originY = this.center.y;
-}
-
-SoundShape.prototype.setGravity = function(grav) {
-    this.gravitySave = this.gravity;
-    this.gravity = grav;
-}
-
-SoundShape.prototype.setShapeColor = function() {
-    var frequency = Math.PI/this.numShapesInGroup;
-    red   = Math.floor(Math.sin(frequency * this.myId + 2) * 127 + 128);
-    green = Math.floor(Math.sin(frequency * this.myId + 0) * 127 + 42);
-    blue  = Math.floor(Math.sin(frequency * this.myId + 4) * 127 + 128);
-    this.myColor = "rgba(" + red + "," + green + "," + blue + ", 0.8)";
-    //console.log("Color is: ",this.myColor);
-}
-
-SoundShape.prototype.updateSoundShapeGravity = function(newData,oldData,time) {
-    this.center.y = 0.5 * this.gravity * Math.pow(time/1000,2);
-    if(this.center.y > (this.height+this.radius)) {
-        this.randomizeRestartTime ?
-        this.setDefaults((new Date()).getTime()+(Math.random()*this.restartTime)):
-        this.setDefaults((new Date()).getTime()+this.restartTime);
-        this.poppedCount = this.poppedCount - 1;
-    }
-    this.processDiff(newData,oldData);
-}
-
-SoundShape.prototype.updateSoundShapeLinear = function(newData,oldData,time) {
-    this.center.y = this.center.y + (this.yVelocity * (time/1000));
-    if(this.center.y > (this.height+this.radius)) {
-        this.randomizeRestartTime ?
-        this.setDefaults((new Date()).getTime()+(Math.random()*this.restartTime)):
-        this.setDefaults((new Date()).getTime()+this.restartTime);
-        this.poppedCount = this.poppedCount - 1;
-    }
-    this.processDiff(newData,oldData);
-}
-
-SoundShape.prototype.updateSoundShapeStatic = function(newData,oldData,time) {
-    this.processDiff(newData,oldData);
-}
-
-SoundShape.prototype.popCircle = function() {
-    this.randomizeRestartTime ?
-    this.setDefaults((new Date()).getTime()+(Math.random()*this.restartTime)):
-    this.setDefaults((new Date()).getTime()+this.restartTime);
-    this.poppedCount++;
-    if(this.poppedCount == this.popMax) {
-        this.gravity = this.gravity + this.gravityOffset;
-        this.poppedCount = 0;
-    }
-
-}
-
-SoundShape.prototype.hitCircle = function() {
-    this.randomizeRestartTime ?
-    this.setDefaults((new Date()).getTime()+(Math.random()*this.restartTime)):
-    this.setDefaults((new Date()).getTime()+this.restartTime);
-    this.poppedCount++;
-    if(this.poppedCount == this.popMax) {
-        this.gravity = this.gravity + this.gravityOffset;
-        this.poppedCount = 0;
-    }
     
 }
 
@@ -193,63 +177,150 @@ SoundShape.prototype.updateSoundShape = function(newData,oldData,time) {
     switch(this.myMotionType) {
         case this.motionType.STATIC:
             this.updateSoundShapeStatic(newData,oldData,time);
-        break;
+            break;
         case this.motionType.LINEAR:
             this.updateSoundShapeLinear(newData,oldData,time);
-        break;
+            break;
         case this.motionType.GRAVITY:
             this.updateSoundShapeGravity(newData,oldData,time);
-        break;
+            break;
     }
-    //call draw routine;
-    this.drawCircle();
 }
 
-
-SoundShape.prototype.setCenter = function(p) {
-  this.center = p;
-  this.currentOriginX = this.center.x;
-  this.currentOriginY = this.center.y;
+SoundShape.prototype.updateSoundShapeGravity = function(newData,oldData,time) {
+    this.center.y = 0.5 * this.gravity * Math.pow(time/1000,2);
+    if(this.center.y > (this.height+this.radius)) {
+        this.randomizeRestartTime ?
+        this.setDefaults((new Date()).getTime()+(Math.random()*this.actionDuration)):
+        this.setDefaults((new Date()).getTime()+this.actionDuration);
+    }
+    this.processGestureTrigger(newData,oldData,time);
 }
 
-SoundShape.prototype.setTone = function(t) {
-    this.tone = t * Math.pow(1.05946,this.Ionian[this.myId]);
-    console.log("Tone is: ",this.tone);
+SoundShape.prototype.updateSoundShapeLinear = function(newData,oldData,time) {
+    this.center.y = this.center.y + (this.yVelocity * (time/1000));
+    if(this.center.y > (this.height+this.radius)) {
+        this.randomizeRestartTime ?
+        this.setDefaults((new Date()).getTime()+(Math.random()*this.actionDuration)):
+        this.setDefaults((new Date()).getTime()+this.actionDuration);
+    }
+    this.processGestureTrigger(newData,oldData,time);
 }
- 
 
-SoundShape.prototype.setRadius = function(r) {
-  this.radius = r;
+SoundShape.prototype.updateSoundShapeStatic = function(newData,oldData,time) {
+    this.processGestureTrigger(newData,oldData,time);
+}
+
+SoundShape.prototype.processGestureTrigger = function(newData, oldData, time) {
+    switch(this.myGestureTriggerType) {
+        case this.gestureTriggerType.POP:
+            //call gesture recognition for a "pop or hit" event, pass in the POP
+            //type so the routine knows which response type to set
+            this.processPopHitDiff(newData,oldData,time,this.gestureResponseType.POP);
+            break;
+        case this.gestureTriggerType.HIT:
+            this.processPopHitDiff(newData,oldData,time,this.gestureResponseType.HIT);
+            break;
+    }
+}
+
+SoundShape.prototype.processGestureResponse = function(time) {
+    switch(this.myGestureResponseType) {
+        case this.gestureResponseType.NONE:
+            this.drawCircle();
+            //console.log("Draw Cirlce: ",this.frameCount);
+            break;
+        case this.gestureResponseType.POP:
+            if(!this.circlePopped) {
+                this.PlayTone(this);
+                this.popCircle();
+            } else {
+                if(time > 0) {
+                    this.PauseTone(this);
+                    this.circlePopped = false;
+                    this.myGestureResponseType = this.gestureResponseType.NONE;
+                }
+            }
+            break;
+        case this.gestureResponseType.HIT:
+            if(!this.circleHit) {
+                this.PlayTone(this);
+                this.hitCircle();
+            } else {
+                if(time > 0) {
+                    this.PauseTone(this);
+                    this.circleHit = false;
+                    this.myGestureResponseType = this.gestureResponseType.NONE;
+                }
+            }
+            this.drawCircle();
+            break;
+    }
+}
+
+SoundShape.prototype.processPopHitDiff = function(diffData, oldData, time, grt) {
+    this.frameCount++;
+    var sum = 0;
+    var yStart = Math.round(this.center.y - this.radius)
+    var yEnd  = Math.round(this.center.y + this.radius);
+    var xStart = Math.round(this.center.x - this.radius);
+    var xEnd  = Math.round(this.center.x + this.radius);
+    //process data if we are not already responding to a previous gesture
+    if(this.myGestureResponseType == this.gestureResponseType.NONE) {
+        for(var i=yStart; i<yEnd; i++) {
+            for(var j=xStart; j<xEnd; j++) {
+                var idx = (j + (i * this.width))*4;
+                if (this.isIn(j,i)) {
+                    sum += ((diffData.data[idx]-oldData.data[idx])*(diffData.data[idx]-oldData.data[idx]));
+                }
+            }
+        }
+        sum = Math.sqrt(sum);
+        if(sum > 0 && !isNaN(sum)) {
+            if(sum > this.diffSumTol) {
+                this.myGestureResponseType = grt;
+            }
+        }
+    }
+    this.processGestureResponse(time);
+}
+
+SoundShape.prototype.popCircle = function() {
+    this.randomizeRestartTime ?
+    this.setDefaults((new Date()).getTime()+(Math.random()*this.actionDuration)):
+    this.setDefaults((new Date()).getTime()+this.actionDuration);
+    this.circlePopped = true;
+}
+
+SoundShape.prototype.hitCircle = function() {
+    this.randomizeRestartTime ?
+    this.setDefaults((new Date()).getTime()+(Math.random()*this.actionDuration)):
+    this.setDefaults((new Date()).getTime()+this.actionDuration);
+    
+    this.circleHit = true;
 }
 
 SoundShape.prototype.drawCircle = function() {
-    if(!this.gestureTriggered) {
-        if(this.playing) {
-            this.PauseTone(this);
-        }
-        this.drawContext.beginPath();
-        this.drawContext.arc(this.center.x,this.center.y,this.radius,0,2.0*Math.PI);
-        switch(this.myColorType) {
-            case this.colorType.SINGLE:
-               this.drawContext.fillStyle = this.myColor;
-            break;
-            case this.colorType.LINEARY:
-                var linGrad = this.drawContext.createLinearGradient(0,0,0,this.height);
-                linGrad.addColorStop(0, "rgba(255,0,0,0.8)");
-                linGrad.addColorStop(0.5,"rgba(0,255,0,0.8)");
-                linGrad.addColorStop(1, "rgba(0,0,255,0.8)");
-                this.drawContext.fillStyle = linGrad;
-            break;
-        }
-        this.drawContext.strokeStyle = "black";
-        this.drawContext.lineWidth = 2.0;
-        this.drawContext.fill();
-        this.drawContext.stroke();
-        this.drawContext.closePath();
-    } else {
-        this.PlayTone(this);
-        this.popCircle();
-    }
+    this.drawContext.beginPath();
+    this.drawContext.arc(this.center.x,this.center.y,this.radius,0,2.0*Math.PI);
+    /*switch(this.myColorType) {
+        case this.colorType.SINGLE:
+            this.drawContext.fillStyle = this.myColor;
+        break;
+        case this.colorType.LINEARY:
+            var linGrad = this.drawContext.createLinearGradient(0,0,0,this.height);
+            linGrad.addColorStop(0, "rgba(255,0,0,0.8)");
+            linGrad.addColorStop(0.5,"rgba(0,255,0,0.8)");
+            linGrad.addColorStop(1, "rgba(0,0,255,0.8)");
+            this.drawContext.fillStyle = linGrad;
+        break;
+    }*/
+    this.drawContext.fillStyle = (this.myGestureResponseType == this.gestureResponseType.NONE) ? this.myColor : this.myHitColor;
+    this.drawContext.strokeStyle = "black";
+    this.drawContext.lineWidth = 2.0;
+    this.drawContext.fill();
+    this.drawContext.stroke();
+    this.drawContext.closePath();
 }
 
 SoundShape.prototype.setFillStyle = function() {
@@ -261,37 +332,6 @@ SoundShape.prototype.setFillStyle = function() {
   }
 
 }
-
-SoundShape.prototype.processDiff = function(diffData, oldData) {
-    this.frameCount++;
-    this.gestureTriggered = false;
-    
-    var sum = 0;
-    var yStart = Math.round(this.center.y - this.radius)
-    var yEnd  = Math.round(this.center.y + this.radius);
-    var xStart = Math.round(this.center.x - this.radius);
-    var xEnd  = Math.round(this.center.x + this.radius);
-    for(var i=yStart; i<yEnd; i++) {
-        for(var j=xStart; j<xEnd; j++) {
-            var idx = (j + (i * this.width))*4;
-            if (this.isIn(j,i)) {
-                sum += ((diffData.data[idx]-oldData.data[idx])*(diffData.data[idx]-oldData.data[idx]));
-            }
-        }
-    }
-    sum = Math.sqrt(sum);
-    
-    if(sum == 0 || isNaN(sum)) {
-        this.diffSum = 0.0;
-        return;
-    } else {
-        this.diffSum = sum;
-        if(this.diffSum > this.diffSumTol) {
-            this.gestureTriggered = true;
-        }
-    }
- }
-
 
 SoundShape.prototype.PlayOrPause = function() {
     this.playing ? this.PauseTone() : this.PlayTone();
@@ -322,9 +362,9 @@ SoundShape.prototype.PlayTone = function (caller) {
        // this.setToneByYLocation();
         this.aSineWave.setFrequency(this.tone);
         this.aSineWave.setAmplitude(this.amplitude);
-        for (var i = 0; i < this.connections.length; i++) {
-            this.aSineWave.getOutNode().connect(this.connections[i]);
-        }
+     //   for (var i = 0; i < this.connections.length; i++) {
+       //     this.aSineWave.getOutNode().connect(this.connections[i]);
+        //}
         this.aSineWave.play();
         //this.setFillStyle();
         this.onFrame = this.frameCount;
